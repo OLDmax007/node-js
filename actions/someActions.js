cabbd5c59d5a86ecffacd5ff137eb4c891eb6b64
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-12-04 13:23:56 +0200

    Overwriting the main branch

--- actions/someActions.js
+++
@@ -1,6 +0,0 @@
-console.log('current path to file:' + __filename)
-
-
-const logWord = (word) => {
-    console.log(word)
-}
--- main.js
+++
@@ -1,5 +0,0 @@
-require('./actions/someActions')
-
-console.log('don')
-console.log('path: ', __dirname)
-console.log('path: ', __filename)
