0932664985efb6bbebb0b86a7ae1b75f991760be
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-12-09 17:50:35 +0200

    module 'fs' with cb for read stream and write him

--- main.js
+++ main.js
@@ -1,37 +1,7 @@
-const {EventEmitter} = require('node:events')
-
-// const userEmitter = new EventEmitter()
-// userEmitter.on('enter_email', (email) => {
-//     console.log(`You have entered ${email}`)
-// })
-// userEmitter.on('enter_data', (data) => {
-//     console.log(`You have entered ${data}`)
-// })
-//
-// userEmitter.emit('enter_email', 'aslsal@gassa.com')
-
-// const userEmitter = new EventEmitter()
-// userEmitter.on('enter_some_data', (data, time) => {
-//     console.log(`You have entered ${data}`, time)
-// })
-// userEmitter.on('enter_some_data', (data, time) => {
-//     console.log(`Your data are ${data}`, time)
-// })
-//
-// userEmitter.emit('enter_some_data', 'Pes', '30:30:30')
-//
+const path = require('node:path')
+const fsCB = require('node:fs')
 
 
-// const userEmitter = new EventEmitter()
-// userEmitter.once('enter_some_data', (data, time) => {
-//     console.log(`You have entered ${data}`, time)
-// })
-// userEmitter.once('enter_some_data', (data, time) => {
-//     console.log(`Your data are ${data}`, time)
-// })
-//
-// userEmitter.emit('enter_some_data', 'Pes', '30:30:30')
-// userEmitter.emit('enter_some_data', 'Pes', '30:30:30')
-// userEmitter.emit('enter_some_data', 'Pes', '30:30:30')
-//
-
+const readStream = fsCB.createReadStream(path.join(process.cwd(), 'conditional-clauses_5513.pdf'))
+const writeStream = fsCB.createWriteStream(path.join(process.cwd(), 'files', 'conditionals.pdf'))
+readStream.pipe(writeStream)
